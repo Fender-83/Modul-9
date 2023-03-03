991b696e2ae13bcae82e5b100344ce7e41b08ed4
Name <fenderpugach@gmail.com>
2023-03-03 11:14:32 +0700

    Решение задания №1

---
+++ Задание 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                Exception[] exc =
+                {
+                new NullExc("Аргумент является null!"),
+                new ArgumentExc("Непустой аргумент!"),
+                new DivideByZeroExc("Деление на ноль недопустимо!"),
+                new FileNotFoundExс("Файл не найден!"),
+                new IndexOutOfRangeExc("Выход за пределы границ массива!")
+            };
+
+                foreach (var error in exc)
+                {
+                    try
+                    {
+                        throw error;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
