793a661e096eae7dc5b8c9b2ec1caf4bba0afb57
Name <fenderpugach@gmail.com>
2023-03-07 11:23:30 +0700

    изменения 2

--- Задание 2/Program.cs
+++ Задание 2/Program.cs
@@ -10,15 +10,19 @@ namespace Задание_2
     {
         static void Main(string[] args)
         {
-            string[] lastname = new string[] { "Иванов", "Петров", "Сидоров", "Кузнецов", "Кузьмин" };
-
+            string[] lastname = new string[5];
+            for (int i = 0; i < lastname.Length; i++)
+            {
+                Console.WriteLine("Введите фмилию " + (i + 1) + "-й персоны");
+                lastname[i] = Console.ReadLine();
+            }
             Event Event = new Event();
             Event.lastnames = lastname;
             Console.Write("Массив: ");
 
             foreach (var name in lastname)
             {
-                Console.Write(lastname + " ");
+                Console.Write(name + " ");
             }
             Event.NumberEnteredEvent += Sort;
             Event.Read();
@@ -31,30 +35,40 @@ namespace Задание_2
             switch (choice)
             {
                 case 1:
-                    Console.WriteLine($"Введено {choice}");
-                    SortMaxMin(lastnames);
-                    break;
-                case 2:
-                    Console.WriteLine($"Введено {choice}");
-                    SortMaxMin(lastnames);
-                    break;
-            }
-        }
-        public static void SortMinMax(string[] lastname)
-        {
-            Array.Sort(lastname);
-            foreach (string str in lastname)
-            {
-                Console.Write(str + " ");
-            }
-        }
+                    foreach (var i in lastnames)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
 
-        public static void SortMaxMin(string[] lastname)
-        {
-            Array.Sort(lastname);
-            foreach (string str in lastname)
-            {
-                Console.Write(str + " ");
+                    Array.Sort(lastnames);
+
+
+                    foreach (var i in lastnames)
+                    {
+                        Console.Write("{0} ", i);
+                    }
+                    Console.WriteLine();
+                    Console.ReadKey();
+                    return;
+
+                case 2:
+                    foreach (var i in lastnames)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+
+                    Array.Sort(lastnames);
+                    Array.Reverse(lastnames);
+
+                    foreach (var i in lastnames)
+                    {
+                        Console.Write("{0} ", i);
+                    }
+                    Console.WriteLine();
+                    Console.ReadKey();
+                    return;
             }
         }
     }
